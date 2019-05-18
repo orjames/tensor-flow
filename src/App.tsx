30048a451ae849b39e41c04e1094100a5ae22678
orjames <orjames@berkeley.edu>
2019-05-18 13:11:27 -0700

    adding ml5

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import tiger from './tiger.jpg';
 import './App.css';
+// import ml5.js
+import * as ml5 from 'ml5';
 
 const App: React.FC = () => {
   return (
     <div className='App'>
       <h1>Image classification with ML5.js</h1>
+      <img src={tiger} id='image' width='400' alt='' />
     </div>
   );
 };
